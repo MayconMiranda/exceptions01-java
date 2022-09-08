6ade4ec27ed9be4a74897832cc973dac1ecd335a
MayconMiranda <maycon12.miranda@gmail.com>
2022-09-08 10:59:44 -0300

    solução boa

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,29 +1,29 @@
 package application;
 
 import model.entities.Reservation;
+import model.exceptions.DomainException;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        System.out.print("numero do quarto: ");
-        int numeroQuarto = leitor.nextInt();
-        System.out.print("data de entrada dd/MM/yyyy: ");
-        Date entrada = sdf.parse(leitor.next());
-        System.out.print("data de saida dd/MM/yyyy: ");
-        Date saida = sdf.parse(leitor.next());
+        try {
+            System.out.print("numero do quarto: ");
+            int numeroQuarto = leitor.nextInt();
+            System.out.print("data de entrada dd/MM/yyyy: ");
+            Date entrada = sdf.parse(leitor.next());
+            System.out.print("data de saida dd/MM/yyyy: ");
+            Date saida = sdf.parse(leitor.next());
 
-        if (!entrada.before(saida)){
-            System.out.println("erro na reserva!,  a data de entrada tem que ser antes da data de saida");
-        }else {
             Reservation reserva = new Reservation(numeroQuarto, entrada, saida);
-
             System.out.println(reserva);
+
             System.out.println();
             System.out.println("insira os dados para atualizar a reserva:");
             System.out.print("data de entrada dd/MM/yyyy: ");
@@ -31,16 +31,20 @@ public class Program {
             System.out.print("data de saida dd/MM/yyyy: ");
             saida = sdf.parse(leitor.next());
 
-
-            String erro = reserva.dataDeAtualizacao(entrada, saida);
-            if (erro != null) {
-                System.out.println("Erro na reserva: " + erro);
-            }
-            else{
-                System.out.println(reserva);
-            }
-
-
+            reserva.dataDeAtualizacao(entrada, saida);
+            System.out.println(reserva);
+        }
+        catch (ParseException e){
+            System.out.println("data invalida! ");
+        }
+        catch (DomainException e){
+            System.out.println("erro na reserva: "+e.getMessage());
+        }
+        catch (InputMismatchException e){
+            System.out.println("erro: Insira numeros inteiros");
+        }
+        catch (RuntimeException e){
+            System.out.println("erro inesperado");
         }
 
         leitor.close();
